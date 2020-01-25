989b4e6eb3e5b77ce9e233227e2927c737aa653f
Frank Tkalcevic <frank@franksworkshop.com.au>
2020-01-25 11:38:59 +1100

    Add license

--- MQTT_Processinglib/JsonNode.cs
+++ MQTT_Processinglib/JsonNode.cs
@@ -1,4 +1,21 @@
-﻿using Newtonsoft.Json;
+﻿// MQTT Processing - MQTT Message event handler and configuration
+// 
+// Copyright(C) 2020  Frank Tkalcevic (frank@franksworkshop.com.au)
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
